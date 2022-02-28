647ac564395812044b5d5b1200ebba2392296662
kemilly <okemilly89@gmail.com>
2022-02-28 16:58:11 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Preparar...");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.ReadKey();
+Console.WriteLine("FOGO!");
